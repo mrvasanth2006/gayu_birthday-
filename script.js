aa01296096da0538fc2b9b2cc4c3a0ad451fe3d0
mrvasanth2006 <pkvtamilan@gmail.com>
2026-07-04 19:42:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-alert("Happy Birthday Gayuu Babyyy ❤️🥳");
+const btn = document.getElementById("startBtn");
+
+btn.addEventListener("click", () => {
+
+    document.body.innerHTML = `
+    
+    <div class="hero">
+    
+        <h1>Hi Gayuu Babyyy...🥹❣️</h1>
+        
+        <p>
+        Indha surprise...
+        Unakkaaga mattum dhaan...🫂💖
+        <br><br>
+        Skip pannama
+        Last varaikkum paakkanum...🤞🏻
+        </p>
+        
+        <button onclick="nextPage()">
+        Promise 🤞🏻
+        </button>
+    
+    </div>
+    
+    `;
+
+});
+
+function nextPage(){
+
+    document.body.innerHTML=`
+    
+    <div class="hero">
+    
+    <h1>Coming Soon...✨</h1>
+    
+    <p>
+    📸 Photo Gallery<br>
+    🎂 Birthday Cake<br>
+    🎉 Confetti<br>
+    💌 Surprise Ending<br><br>
+    
+    Website construction is in progress...🥹❣️
+    
+    </p>
+    
+    </div>
+    
+    `;
+
+}

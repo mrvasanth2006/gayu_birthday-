a4d7c27f6034b42ec2379ade7bc47e5a863cde3c
mrvasanth2006 <pkvtamilan@gmail.com>
2026-07-05 13:36:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,50 +2,75 @@ const btn = document.getElementById("startBtn");
 
 btn.addEventListener("click", () => {
 
-    document.body.innerHTML = `
-    
-    <div class="hero">
-    
-        <h1>Hi Gayuu Babyyy...🥹❣️</h1>
-        
-        <p>
-        Indha surprise...
-        Unakkaaga mattum dhaan...🫂💖
-        <br><br>
-        Skip pannama
-        Last varaikkum paakkanum...🤞🏻
-        </p>
-        
-        <button onclick="nextPage()">
-        Promise 🤞🏻
-        </button>
-    
-    </div>
-    
-    `;
+document.body.innerHTML = `
+
+<div class="container">
+
+<h1 style="font-size:2.8rem;">
+Hi Gayuu Babyyy...🥹❣️
+</h1>
+
+<p style="margin-top:20px;font-size:20px;line-height:1.8;">
+Indha surprise...
+<br><br>
+Unakkaaga mattum dhaan...🫂💖
+<br><br>
+Skip pannama
+Last varaikkum paakkanum...🤞🏻
+</p>
+
+<br>
+
+<button id="promiseBtn">
+Promise 🤞🏻
+</button>
+
+</div>
+
+`;
+
+const promiseBtn = document.getElementById("promiseBtn");
+
+promiseBtn.style.padding = "15px 35px";
+promiseBtn.style.border = "none";
+promiseBtn.style.borderRadius = "50px";
+promiseBtn.style.background = "#ff4da6";
+promiseBtn.style.color = "white";
+promiseBtn.style.fontSize = "20px";
+promiseBtn.style.cursor = "pointer";
+promiseBtn.style.boxShadow = "0 0 20px #ff4da6";
+
+promiseBtn.addEventListener("click", memories);
 
 });
 
-function nextPage(){
+function memories(){
 
-    document.body.innerHTML=`
-    
-    <div class="hero">
-    
-    <h1>Coming Soon...✨</h1>
-    
-    <p>
-    📸 Photo Gallery<br>
-    🎂 Birthday Cake<br>
-    🎉 Confetti<br>
-    💌 Surprise Ending<br><br>
-    
-    Website construction is in progress...🥹❣️
-    
-    </p>
-    
-    </div>
-    
-    `;
+document.body.innerHTML = `
+
+<div class="container">
+
+<h1>Our Memories 🥹❣️</h1>
+
+<img id="slide"
+src="20260223_180501.jpg"
+style="
+width:300px;
+height:400px;
+object-fit:cover;
+border-radius:20px;
+box-shadow:0 0 30px #ff4da6;
+margin-top:20px;
+">
+
+<p style="margin-top:25px;font-size:20px;">
+Every photo with you...🫂❣️
+<br><br>
+is my favourite memory...🥹
+</p>
+
+</div>
+
+`;
 
 }
